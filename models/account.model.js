1ab117f77708fb28ec3cfe2c11467954ac8e134e
khanhhln202 <khanhhln2201@gmail.com>
2025-03-18 21:13:48 +0700

    Create accounts collection

---
+++ models/account.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const generateToken = require('../helpers/generateToken');
+
+const accountSchema = new mongoose.Schema({
+    fullName: String,
+    email: String,
+    password: String,
+    token: {
+        type: String,
+        default: generateToken.generateRandomString(32)
+    },
+    phone: String,
+    address: String,
+    avatar: String,
+    role_id: String,
+    deleted: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt: Date,
+}, {timestamps: true});
+
+const Account = mongoose.model('Account', accountSchema, 'accounts');
+
+module.exports = Account;
